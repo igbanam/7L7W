7969ab5ca40f792e750d5c07aa761ac90760231f
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-05-29 15:15:28 -0400

    up to LPN exercise 6.5 [partially incomplete]
    at e 6.5, my predicate is recursively obnoxious!

--- prolog/README.md
+++ prolog/README.md
@@ -5,8 +5,12 @@ These are notes I took while learning prolog. Most of these are
 knowledge bases directly lifted from the resources listed below.
 The rest are my solutions to problems in the tutorials.
 
+The interpreter I use us [GNUProlog][1].
+
 ###Copyright
 
 Copyright? No
 Copyleft?  No
 Just Copy? Yes!
+
+[1]: www.gprolog.org
--- prolog/learnprolognow/chapter6.pl
+++ prolog/learnprolognow/chapter6.pl
@@ -3,7 +3,10 @@
 % append/3 ... append(A, B, C).
 % Appends A to B and returns it in C.
 % below is a naive implementation. GNUProlog has a 
-% faster O(1) implementation rightly named
+% faster O(1) implementation rightly named "append"
+
+naive_append([], L, L).
+naive_append([H | T], L2, [H | L3]) :- naive_append(T, L2, L3).
 
 % prefix/2 ... prefix(P, L) :- append(P, _, L);
 % There exists some list which when P is appended to
@@ -17,5 +20,59 @@
 % to which SubL is a prefix.
 % sublist(SubL, L) :-  suffix(S, L), prefix(SubL,S).
 
+/* reversing the list by using multiple recursive
+ * naive_append calls
+ */
 naiverev([],[]). 
-naiverev([H | T], R) :- naiverev(T, RevT), append(RevT, [H], R).
+naiverev([H | T], R) :- naiverev(T, RevT), naive_append(RevT, [H], R).
+
+/* Using an accumualator is invariably faster as it 
+ * reduces the passes made down to one.
+ */
+accRev([H | T], A, R) :- accRev(T, [H | A], R). 
+accRev([], A, A).
+
+%%%%%%%%%%%%%%%
+%% EXERCISES %%
+%%%%%%%%%%%%%%%
+
+% 6.1
+doubled(L) :-
+	length(L, G), % Original list length
+	0 is G mod 2, % ... is even
+	prefix(P, L), % There is some prefix...
+	length(P, PL), % ...whose length...
+	PL is G // 2, % ...is half the original list
+	suffix(S, L), % There is some suffix...
+	length(S, SL), % ...whose length...
+	SL is G // 2, % ...is half the original list
+	P = S. % The prefix and suffix are the same
+
+% 6.2 - Palindromes
+palindrome(L) :- reverse(L, L). 
+% can be done with naiverev
+
+% 6.3 - TopTail
+toptail(I, O) :-
+	length(I, Li), % Length of the inList...
+	Li >= 2, % ...is 2 or more.
+	[H|_] = I, % The head of the list
+	delete(I, H, K), % ...should be removed
+	reverse(K, J), % reverse the new list
+	[H1|_] = J, % The head of the list (ie last of original)
+	delete(J, H1, M), % ...should be removed 
+	reverse(M, O).
+
+% 6.4 - Last 1 - using reverse
+vlast(L, E) :- reverse(L, [E | _]).
+
+% 6.4 - Last 2 - using recursion
+rlast([X], X).
+rlast([H | T], E) :- rlast(T, E).
+
+% 6.5 - Swap first and last elements
+swapfl(A, B) :-
+	[AH | _] = A,
+	[BH | _] = B,
+	rlast(A, BH),
+	rlast(B, AH).
