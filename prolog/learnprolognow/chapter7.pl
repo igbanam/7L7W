385d57f91cee2a8e1beec0f17cf8d7ec62707ae8
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-06-03 11:28:07 -0400

    started CFGs

--- prolog/learnprolognow/chapter7.pl
+++ prolog/learnprolognow/chapter7.pl
@@ -15,3 +15,42 @@ n([woman]).
 n([man]). 
  
 v([shoots]).
+
+% this isn't optimal, it reaches some stack overflow
+
+% the consumer paradigm...
+
+cs(X, Z) :- cnp(X, Y),  cvp(Y, Z). 
+ 
+cnp(X, Z):- cdet(X, Y),  cn(Y, Z). 
+ 
+cvp(X, Z):- cv(X, Y),  cnp(Y, Z). 
+ 
+cvp(X, Z):- cv(X, Z). 
+ 
+cdet([the|W],W). 
+cdet([a|W],W). 
+
+cn([woman|W],W). 
+cn([man|W],W). 
+
+cv([shoots|W],W).
+
+% syntactic sugar for above
+% I verified this with trace.
+
+dcg_s  -->  dcg_np, dcg_vp. 
+ 
+dcg_np  -->  dcg_det, dcg_n. 
+ 
+dcg_vp  -->  dcg_v, dcg_np. 
+dcg_vp  -->  dcg_v. 
+ 
+dcg_det  -->  [the]. 
+dcg_det  -->  [a]. 
+ 
+dcg_n  -->  [woman]. 
+dcg_n  -->  [man]. 
+ 
+dcg_v  -->  [shoots].
+
