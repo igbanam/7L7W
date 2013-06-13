34174f054928ec2ecf243b58f211694f08f34054
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-06-13 14:09:33 -0400

    atom koan enlightened

--- erlang/erlang-koans-master/src/about_atoms.erl
+++ erlang/erlang-koans-master/src/about_atoms.erl
@@ -3,14 +3,14 @@
 -include("../resources/koans.hrl").
 
 truth() ->
-  ?ReplaceMe =:= true.
+  true =:= true.
 
 truth_is_not_always_boolean() ->
-  is_atom(true) =:= ?ReplaceMe.
+  is_atom(true) =:= true.
 
 strings_are_not_atoms() ->
-  is_atom("true") =:= ?ReplaceMe.
+  is_atom("true") =:= false.
 
 however_atoms_might_look_like_a_string() ->
-  is_atom('true') =:= ?ReplaceMe.
+  is_atom('true') =:= true.
 
