36c841be0506d7ca972ae5384a532d9639d93483
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-06-13 14:34:32 -0400

    boolean expression koan complete

--- erlang/erlang-koans-master/src/about_boolean_expressions.erl
+++ erlang/erlang-koans-master/src/about_boolean_expressions.erl
@@ -3,36 +3,36 @@
 -include("../resources/koans.hrl").
 
 or_operator() ->
-  ?ReplaceMe =:= true or false.
+  true =:= true or false.
 
 and_operator() ->
-  ?ReplaceMe =:= true and false.
+  false =:= true and false.
 
 xor_operator() ->
-  ?ReplaceMe =:= true xor false.
+  true =:= true xor false.
 
 not_operator() ->
-  ?ReplaceMe =:= not true.
+  false =:= not true.
 
 being_strict() ->
   StrictlyDefined = (42 =:= 42.0),
-  ?ReplaceMe =:= StrictlyDefined.
+  false =:= StrictlyDefined.
 
 being_not_so_strict() ->
   NotSoStrictlyDefined = (42 == 42.0),
-  ?ReplaceMe =:= NotSoStrictlyDefined.
+  true =:= NotSoStrictlyDefined.
 
 special_words_evaluate_only_what_is_necessary() ->
   ShortCircuit = (true andalso false),
-  ?ReplaceMe =:= ShortCircuit.
+  false =:= ShortCircuit.
 
 this_applies_to_or_as_well() ->
   ShortCircuit = (false orelse true),
-  ?ReplaceMe =:= ShortCircuit.
+  true =:= ShortCircuit.
 
 make_de_morgan_proud() ->
-  ((not true) orelse (?ReplaceMe)) =:= (not (false andalso true)).
+  ((not true) orelse (true)) =:= (not (false andalso true)).
 
 syntax_you_might_not_expect() ->
-  ?ReplaceMe =:= (1 =< 2) and (3 /= 4).
+  true =:= (1 =< 2) and (3 /= 4).
 
