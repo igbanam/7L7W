172e27ab2a6cdcc30fb53373389a0d6772471a89
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-06-13 15:51:42 -0400

    binary koan complete

--- erlang/erlang-koans-master/src/about_binaries.erl
+++ erlang/erlang-koans-master/src/about_binaries.erl
@@ -3,14 +3,14 @@
 -include("../resources/koans.hrl").
 
 binaries_of_strings() ->
-  ?ReplaceMe =:= binary:bin_to_list(<<"What am I, really?">>).
+  "What am I, really?" =:= binary:bin_to_list(<<"What am I, really?">>).
 
 and_also_strings_of_binaries() ->
-  ?ReplaceMe =:= binary:list_to_bin("What am I, really?").
+  <<"What am I, really?">> =:= binary:list_to_bin("What am I, really?").
 
 the_first_byte_is_a_byte___right() ->
-  ?ReplaceMe =:= binary:first(<<"binary">>).
+  98 =:= binary:first(<<"binary">>).
 
 then_what_is_the_last() ->
-  ?ReplaceMe =:= binary:last(<<"binary">>).
+  121 =:= binary:last(<<"binary">>).
 
