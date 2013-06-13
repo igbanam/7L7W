a212494fd0e10c156245529894d2e735169dfc94
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2013-06-13 16:01:54 -0400

    guards koan'd

--- erlang/erlang-koans-master/src/about_guard_clauses.erl
+++ erlang/erlang-koans-master/src/about_guard_clauses.erl
@@ -3,7 +3,7 @@
 -include("../resources/koans.hrl").
 
 truthfulness_is_necessary_for_guards() ->
-  is_this_argument_truthy(?ReplaceMe).
+  is_this_argument_truthy(true).
 
 is_this_argument_truthy(X) when X ->
   get_here;
@@ -11,7 +11,7 @@ is_this_argument_truthy(_) ->
   not_here.
 
 there_exists_a_shorthand_for_and() ->
-  and_shorthand(?ReplaceMe).
+  and_shorthand(0).
 
 and_shorthand(X) when is_integer(X), X < 2 ->
   get_here;
@@ -19,7 +19,7 @@ and_shorthand(_) ->
   not_here.
 
 there_also_exists_a_shorthand_for_or() ->
-  or_shorthand(?ReplaceMe).
+  or_shorthand(1.0).
 
 or_shorthand(X) when is_integer(X); is_float(X) ->
   get_here;
